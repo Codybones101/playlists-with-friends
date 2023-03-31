9db4ee7c6fdac1048c738652705a6f52597e4ab1
CodyBones101 <iamaplsoz@gmail.com>
2023-03-31 13:03:35 -0400

    style update

--- src/pages/Home/homepage.jsx
+++ src/pages/Home/homepage.jsx
@@ -1,5 +1,10 @@
+import "./homepage.css"
+
 export default function Homepage() {
     return (
-        <h1>Homepage</h1>
+        <div className="homediv">
+        <h1 className="home-text">Welcome to Playlists With Friends!</h1>
+        <h2 className="secondary">If you've ever wanted to listen to your favorite music from different web players in the same location, you've come to the right place! Create playlists using embed links from Youtube, Spotify or Soundcloud to share a multi-web player compilation of tunes!</h2>
+        </div>
     )
 }
